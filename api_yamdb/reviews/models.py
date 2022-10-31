a1e3b5f4be27466d8f4c53d44c9ced9599b5a12a
Евгений Привалов <evprivalov86@gmail.com>
2022-10-31 20:07:51 +0500

    Commit_1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,6 @@
-from email.policy import default
-from django.db import models
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator, MaxValueValidator
-
-from users.models import USER
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 from .validators import validate_year
 
